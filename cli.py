5f13a134c8168b2ea2815b684085ee1b4df6dab0
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-11-14 17:28:52 -0500

    implement positive_float in cli.py

--- cli.py
+++ cli.py
@@ -1,2 +1,18 @@
+import sys
+from argparse import ArgumentParser, RawDescriptionHelpFormatter, ArgumentTypeError
+
+
+def positive_float(value):
+    """An argparse-like method for accepting only positive floats"""
+    try:
+        fvalue = float(value)
+    except (ValueError, TypeError) as e:
+        raise ArgumentTypeError('Expected a positive float, error message: '
+                                '{}'.format(e))
+    if fvalue <= 0:
+        raise ArgumentTypeError('Expected a positive float')
+    return fvalue
+
+
 def main():
     raise NotImplementedError()
