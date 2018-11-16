855242b15b386c58b7f7fd1fb13318ff4f5bf5e2
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-11-16 10:52:55 -0500

    write get_output_path

--- birdvoxdetect/core.py
+++ birdvoxdetect/core.py
@@ -3,3 +3,30 @@ TARGET_SR = 22050
 def process_file(filepath,
         output_dir=None, suffix=None, hop_size=0.05, verbose=True):
     raise NotImplementedError()
+
+
+def get_output_path(filepath, suffix, output_dir=None):
+    """
+    Parameters
+    ----------
+    filepath : str
+        Path to audio file to be processed
+    suffix : str
+        String to append to filename (including extension)
+    output_dir : str or None
+        Path to directory where file will be saved. If None, will use directory of given filepath.
+    Returns
+    -------
+    output_path : str
+        Path to output file
+    """
+    base_filename = os.path.splitext(os.path.basename(filepath))[0]
+    if not output_dir:
+        output_dir = os.path.dirname(filepath)
+
+    if suffix[0] != '.':
+        output_filename = "{}_{}".format(base_filename, suffix)
+    else:
+        output_filename = base_filename + suffix
+
+    return os.path.join(output_dir, output_filename)
