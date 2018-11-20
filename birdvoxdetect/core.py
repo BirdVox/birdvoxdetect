0735c02aeb0a722a6ac9bcabb3874a25df3b6cb5
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-11-20 18:37:37 -0500

    load input in process_file

--- birdvoxdetect/core.py
+++ birdvoxdetect/core.py
@@ -1,9 +1,35 @@
 import os
+import soundfile as sf
 
 TARGET_SR = 22050
 
+def _center_audio(audio, frame_len):
+    """Center audio so that first sample will occur in the middle of the first frame"""
+    return np.pad(audio, (int(frame_len / 2.0), 0), mode='constant', constant_values=0)
+
+
 def process_file(filepath,
-        output_dir=None, suffix=None, hop_size=0.05, verbose=True):
+        output_dir=None,
+        export_clips=False,
+        threshold=50.0,
+        suffix="",
+        frame_rate=20.0,
+        clip_duration=1.0,
+        verbose=True):
+    
+    # Check for existence of the file.
+    if not os.path.exists(filepath):
+        raise BirdVoxDetectError('File "{}" could not be found.'.format(filepath))
+        
+    # Try loading the file as NumPy array.
+    try:
+        audio, sr = sf.read(filepath)
+    except Exception:
+        raise OpenL3Error('Could not open file "{}":\n{}'.format(filepath, traceback.format_exc()))
+        
+    # Define output path.
+    output_path = get_output_path(filepath, suffix + ".csv", output_dir=output_dir)
+
     raise NotImplementedError()
 
 
