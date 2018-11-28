ec543397bca34ed8265e1db3630f3ee1494c5c01
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-11-28 13:12:44 -0500

    restore py2

--- setup.py
+++ setup.py
@@ -63,6 +63,8 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
