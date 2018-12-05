a5be42a08cd63b3eada83fc169bde9597a445323
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-12-05 10:16:59 -0500

    bugfix export_clips

--- birdvoxdetect/core.py
+++ birdvoxdetect/core.py
@@ -312,16 +312,17 @@ def process_file(
         df.to_csv(timestamps_path, index=False)
 
         # Export clips.
-        for t in th_peak_timestamps:
-            clip_start = max(0, int(sr*np.round(t-0.5*clip_duration)))
-            clip_stop = min(
-                len(sound_file), int(sr*np.round(t+0.5*clip_duration)))
-            sound_file.seek(clip_start)
-            audio_clip = sound_file.read(clip_stop-clip_start)
-            clip_name = suffix + "{:05.2f}".format(t).replace(".", "-")
-            clip_path = get_output_path(
-                filepath, clip_name + ".wav", output_dir=clips_dir)
-            librosa.output.write_wav(clip_path, audio_clip, sr)
+        if export_clips:
+            for t in th_peak_timestamps:
+                clip_start = max(0, int(sr*np.round(t-0.5*clip_duration)))
+                clip_stop = min(
+                    len(sound_file), int(sr*np.round(t+0.5*clip_duration)))
+                sound_file.seek(clip_start)
+                audio_clip = sound_file.read(clip_stop-clip_start)
+                clip_name = suffix + "{:05.2f}".format(t).replace(".", "-")
+                clip_path = get_output_path(
+                    filepath, clip_name + ".wav", output_dir=clips_dir)
+                librosa.output.write_wav(clip_path, audio_clip, sr)
 
     # Export likelihood curve.
     if export_likelihood:
