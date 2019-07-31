ea2e62b08fe8c24e39198f02fcf89e8fc227ede1
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2019-07-30 20:42:56 -0400

    classify_species(chunk_pcen, th_peak_loc):

--- birdvoxdetect/core.py
+++ birdvoxdetect/core.py
@@ -573,14 +573,13 @@ def process_file(
         logging.info(event_str.format(confidence_path))
 
 
-def classify_species(chunk_pcen, th_peak_timestamp):
+def classify_species(chunk_pcen, th_peak_loc):
     # Load settings
     pcen_settings = get_pcen_settings()
     clip_length = 104
 
-    # Convert timestamp to hop
-    th_peak_hop = (th_peak_timestamp *\
-        pcen_settings["sr"]/pcen_settings["hop_length"]).astype('int')
+    # Convert birdvoxdetect hops to PCEN hops
+    th_peak_hop = th_peak_loc * pcen_settings["stride_length"]
 
     # We recenter the clip around the flight call by a half-stride
     th_peak_hop = th_peak_hop - pcen_settings["stride_length"]//2
