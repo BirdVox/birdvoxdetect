7a70a36fc9828d63f5e4f4a6d3d36087137f58dd
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2019-07-31 11:08:28 -0400

    logging._warn_preinit_stderr = 0

--- birdvoxdetect/__init__.py
+++ birdvoxdetect/__init__.py
@@ -3,6 +3,7 @@ import logging
 
 formatter = logging.Formatter("%(message)s")
 absl.logging.get_absl_handler().setFormatter(formatter)
+absl.logging._warn_preinit_stderr = 0
 
 from .version import version as __version__
 from .core import process_file
