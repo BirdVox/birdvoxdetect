14bef42b5b8bd91ed80f3fd788b8aec7fa493c5b
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2019-09-25 10:33:13 -0400

    update map_confidence

--- birdvoxdetect/core.py
+++ birdvoxdetect/core.py
@@ -845,27 +845,11 @@ def get_model_path(model_name):
 
 
 def map_confidence(y, model_name):
-    if model_name == "birdvoxdetect-v03_T-1800_trial-37_network_epoch-023":
-        # Uncalibrated! (TODO)
-        # See repository: github.com/BirdVox/birdvox-full-season
-        # Notebook: detector/notebooks/BirdVoxDetect-v01_calibration.ipynb
-        # This model encodes "0" resp. "1" as "event" resp. "no event"
-        log1my = np.log1p(np.clip(-y, np.finfo(np.float32).eps - 1, None))
-        logy = np.log(np.clip(y, np.finfo(np.float32).tiny, None))
-        y_inverse_sigmoid = log1my - logy
-        y_out = 6 * y_inverse_sigmoid
-    elif model_name == "birdvoxdetect-v03_trial-12_network_epoch-068":
-        # Uncalibrated! (TODO)
-        # See repository: github.com/BirdVox/birdvox-full-season
-        # Notebook: detector/notebooks/BirdVoxDetect-v01_calibration-nocontext.ipynb
-        # This model encodes "1" resp. "0" as "event" resp. "no event"
-        log1my = np.log1p(np.clip(-y, np.finfo(np.float32).eps - 1, None))
-        logy = np.log(np.clip(y, np.finfo(np.float32).tiny, None))
-        y_inverse_sigmoid = log1my - logy
-        y_out = y_inverse_sigmoid
-    else:
-        y_out = y
-    return y_out
+    log1my = np.log1p(np.clip(-y, np.finfo(np.float32).eps - 1, None))
+    logy = np.log(np.clip(y, np.finfo(np.float32).tiny, None))
+    y_inverse_sigmoid = log1my - logy
+    y_out = y_inverse_sigmoid - np.log(np.finfo(np.float32).eps)
+    return np.clip(y_out, 0, 100)
 
 
 def seconds_to_hhmmss(total_seconds):
