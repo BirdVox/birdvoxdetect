f6fe2ca12df79cddd1f01ec2336187b7f4c40707
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2019-12-02 20:53:49 -0500

    bugfix local variable 'concat_deque' referenced before assignment

--- birdvoxdetect/core.py
+++ birdvoxdetect/core.py
@@ -209,7 +209,7 @@ def process_file(
         deque.append(chunk_pcen)
 
     # Compute context.
-    if has_context and (n_chunks>1):
+    if (n_chunks>1):
         concat_deque = np.concatenate(deque, axis=1)
         deque_context = np.percentile(
             concat_deque, percentiles, axis=1, overwrite_input=True)
